287e73f46a16bda57d5346bc3fca30117f655bca
GitStux <63031851+GitStux@users.noreply.github.com>
2020-11-30 23:14:37 -0800

    forms and onclick

---
+++ script.js
@@ -0,0 +1,42 @@
+document.getElementById("myForm").addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    let newPar = document.createElement("p")
+
+    newPar.innerHTML = event.target.elements["message"].value;
+    document.getElementById("boldWall").appendChild(newPar)
+    newPar.style.border = "3px solid"
+    newPar.style.borderColor = randomColors()
+
+
+    confirm("Are you sure about this post:" + " " + newPar.innerHTML)
+})
+
+let theTitle = document.getElementById("title")
+
+theTitle.onclick = changeTitle
+
+
+function changeTitle() {
+    theTitle.style.backgroundColor = randomColors();
+
+}
+
+let colors = {
+
+    1: "blue",
+    2: "green",
+    3: "orange",
+    4: "purple"
+
+}
+
+function randomColors() {
+    let rand = Math.floor(Math.random() * 4) + 1
+
+    for (prop in colors) {
+        if (rand == prop) {
+            return colors[prop]
+        }
+    }
+}
